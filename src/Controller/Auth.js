7fc4d933d87c094a7bb4889eb1283010cd0def3a
tasammie <temiloluwasamuel4@gmail.com>
2024-11-12 14:01:08 +0100

    new changes

--- src/Controller/Auth.js
+++ src/Controller/Auth.js
@@ -11,20 +11,16 @@ const signJWTToken = (id) => {
 
 const register = async (req, res) => {
   try {
-    const { firstName, lastName, email, password, confirmPassword } = req.body;
+    const { firstName, lastName, email, password } = req.body;
 
-    if (!firstName || !lastName || !email || !password || !confirmPassword) {
-      return res.status(400).json({ msg: "Please fill all fields" });
+    if (!firstName || !lastName || !email || !password) {
+      return res.status(400).json({ message: "Please fill all fields" });
     }
-
-    if (password !== confirmPassword) {
-        return res.status(400).json({ msg: "Passwords do not match" });
-      }
     const existingUser = await User.findOne({ email });
     if (existingUser) {
-      return res.status(400).json({ msg: "User already exists" });
+      return res.status(409).json({ message: "User already exists" });
     }
-
+    // Hash the password
     const salt = await bcrypt.genSalt(12);
     const hashedPassword = await bcrypt.hash(password, salt);
 
@@ -36,47 +32,51 @@ const register = async (req, res) => {
     });
 
     if (!user) {
-      return res.status(404).json({ msg: "Failed to create user" });
+      return res.status(500).json({ message: "Failed to create user" });
     }
-
+    // Create and assign a JWT token to the user
     const token = signJWTToken(user._id);
-    res.status(200).json({
+    res.status(201).json({
       status: "success",
       user,
       token,
     });
-
   } catch (error) {
     console.error("Error creating user:", error);
-    return res.status(500).json({ msg: "Server error" });
+    return res.status(500).json({ message: "Server error" });
   }
 };
+
+
 
 const login = async (req, res) => {
   try {
     const { email, password } = req.body;
-
     if (!email || !password) {
       return res.status(400).json({ msg: "Please fill all fields" });
     }
 
     const user = await User.findOne({ email });
     if (!user) {
-      return res.status(404).json({ msg: "User not found" });
+      return res.status(401).json({ msg: "User not found" });
     }
 
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
-      return res.status(400).json({ msg: "Invalid password" });
+      return res.status(401).json({ msg: "Invalid password" });
     }
 
     const token = signJWTToken(user._id);
     res.status(200).json({
       status: "user login successful",
-      user,
+      user: {
+        firstName: user.firstName,
+        lastName: user.lastName,
+        email: user.email,
+        id: user._id,
+      },
       token,
     });
-
   } catch (error) {
     console.error("Error logging in user:", error);
     return res.status(500).json({ msg: "Server error" });
